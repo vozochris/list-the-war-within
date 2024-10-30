10e0807ce9ff8bb279a53e44d7278db42df19ae5
Cristian Vozoca <vozochris@gmail.com>
2024-10-30 18:07:55 +0100

    v1.0.80

--- 20AnniversaryOnInit.lua
+++ 20AnniversaryOnInit.lua
@@ -1,37 +1,38 @@
 aura_env.tanaris = {
-    {name = "Meet and Greet", quests = {84254}},
-    {name = "Shopping Squee!", quests = {84489}},
-    {name = "Truth of the Dark Irons", quests = {84735}},
+    {name = "An Original Path Through Time", quests = {83274, 85947}},
     {name = "Celebrate Good Fun!", quests = {84616}},
     {name = "Chromie's Codex", quests = {82783}},
-    {name = "An Original Path Through Time", quests = {83274, 85947}},
     {name = "Disturbance Detected: Blackrock Depths", quests = {82817}},
+    {name = "Meet and Greet", quests = {84254}},
+    {name = "Shopping Squee!", quests = {84489}},
     {name = "Tat Big Meanie", quests = {84144}},
-    {name = "Tweasure Hunt", quests = {84363}},
+    {name = "The \"Great\" Detective", quests = {84237}},
     {name = "Thoughtful Pursuits", quests = {84521}},
+    {name = "Truth of the Dark Irons", quests = {84735}},
+    {name = "Tweasure Hunt", quests = {84363}},
     {name = "World Boss Quest", quests = {60215, 47254}},
 }
 
 aura_env.tww = {
-    {id = "dornogal_meta_quest", name = "Delves: Percussive Archaeology", quests = {82709}},
+    {id = "azj_kahet_meta_quest", name = "Azj-Kahet Meta Quest", quests = {80670, 80671, 80672}},
+    {id = "dornogal_meta_quest", name = "Dornogal Meta Quest", quests = {82452, 82482, 82496, 82512}},
     {id = "gearing_up_for_trouble", name = "Gearing Up for Trouble", quests = {83333}},
+    {id = "queen_ansurek", name = "Queen Ansurek", quests = {84597, 84276}},
     {id = "rollin_down_in_the_deeps", name = "Rollin' Down in the Deeps", quests = {82946}},
+    {id = "special_assignment", name = "Special Assignment", quests = {82355, 81649, 81647, 81650, 81691, 83229, 82852, 82787, 82414, 82531}},
     {id = "spreading_the_light", name = "Spreading the Light", quests = {76586}},
     {id = "the_theater_troupe", name = "The Theater Troupe", quests = {83240}},
-    {id = "azj_kahet_meta_quest", name = "Azj-Kahet Meta Quest", quests = {80670, 80671, 80672}},
     {id = "world_boss", name = "World Boss", quests = {83466, 83467, 81624, 81630}},
-    {id = "queen_ansurek", name = "Queen Ansurek", quests = {84597, 84276}},
 }
 
 aura_env.celebration_crates = {
-    {name = "Soggy Celebration Crate", quests = {83794}, info = "Zuldazar, underwater, buy a Clam Digger from Nikto and click on Gerald above", coords = "54205420"},
     {name = "Hazy Celebration Crate", quests = {85574}, info = "Desolace, be a ghost/dead to see it", coords = "54005810"},
+    {name = "Soggy Celebration Crate", quests = {83794}, info = "Zuldazar, underwater, buy a Clam Digger from Nikto and click on Gerald above", coords = "54205420"},
 }
 
 aura_env.pvp = {
+    {name = "Preserving", quests = {80184, 80185, 80186, 80187, 80188, 80189}, completedOn = 2},
     {name = "Soldier of Time", quests = {57300}},
-    {name = "Sparks of War: Hallowfall", quests = {81795}},
-    {name = "Preserving in Skirmishes", quests = {80187}},
-    {name = "Preserving in Arenas", quests = {80188}},
     {name = "Something Different", quests = {47148}},
+    {name = "Sparks of War", quests = {81793, 81794, 81795, 81796}},
 }
