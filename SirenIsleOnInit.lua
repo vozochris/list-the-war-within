6f6affb1a74a54bf2c1958e1766961dcc83c06be
Cristian Vozoca <vozochris@gmail.com>
2024-12-26 15:30:19 +0100

    v1.0.102

--- SirenIsleOnInit.lua
+++ SirenIsleOnInit.lua
@@ -10,41 +10,37 @@ aura_env.siren_isle = {
 }
 
 aura_env.siren_isle_rares = {
-    {name = "Asbjorn the Bloodsoaked", quests = {}, coords = "63948729"},
+    {name = "Asbjorn the Bloodsoaked", quests = {84805}, coords = "63948729"},
     {name = "Bloodbrine", quests = {84795}, coords = "36147261"},
     {name = "Brinebough", quests = {85404}, coords = "55968404"},
     {name = "Chef Chum Platter", quests = {84800}, coords = "66128495"},
-    {name = "Coralweaver Calliso", quests = {}, coords = "61758953"},
+    {name = "Coralweaver Calliso", quests = {84802}, coords = "61758953"},
     {name = "Ghostmaker", quests = {84801}, coords = "53283383"},
-    {name = "Gravesludge", quests = {}, coords = "57726612"},
+    {name = "Gravesludge", quests = {85669}, coords = "57726612"},
     {name = "Grimgull", quests = {84796}, coords = "34151392"},
-    {name = "Ikir the Flotsurge", quests = {}, coords = "32457405"},
-    {name = "Nerathor", quests = {}, coords = "26206546"},
+    {name = "Ikir the Flotsurge", quests = {84792}, coords = "32457405"},-- 84792 OR 84839
+    {name = "Nerathor", quests = {85672}, coords = "26206546"},
     {name = "Plank-Master Bluebelly", quests = {84799}, coords = "60698921"},
     {name = "Siris the Sea Scorpion", quests = {84803}, coords = "56068410"},
     {name = "Slaughtershell", quests = {84798}, coords = "50005000"},
     {name = "Snacker", quests = {86933}, coords = "67391919"},
-    {name = "Stalagnarok", quests = {}, coords = "37115497"},
-    {name = "Tempest Talon", quests = {}, coords = "42976324"},
+    {name = "Stalagnarok", quests = {85437}, coords = "37115497"},
+    {name = "Tempest Talon", quests = {85403}, coords = "42976324"},
     {name = "Wreckwater", quests = {84794}, coords = "46787812"},
-    {name = "Zek'ul the Shipbreaker", quests = {}, coords = "33027359"},
+    {name = "Zek'ul the Shipbreaker", quests = {85405}, coords = "33027359"},
 }
 
 aura_env.siren_isle_treasures = {
-    {name = "Barnacle-Encrusted Chest", quests = {}, coords = "74035329"},
-    {name = "Bilge Rat Supply Chest", quests = {}, coords = "62449084"},
-    {name = "Empty Kaja'Cola", quests = {}, coords = "60726280"},
+    {name = "Barnacle-Encrusted Chest", quests = {86765}, coords = "74035329"},
+    {name = "Empty Kaja'Cola", quests = {84547}, coords = "60726280"},
     {name = "Iron Mining Pick", quests = {86765}, coords = "36925304"},
-    {name = "Kul Tiran Lumberer's Hatchet", quests = {}, coords = "40284188"},
-    {name = "Mouldy Sea Ledger", quests = {}, coords = "39785249"},
-    {name = "Rune-Seared Spear", quests = {}, coords = "57937837"},
-    {name = "Scholars of the Sea: Siren Isle Synopsis", quests = {}, coords = "39095103"},
+    {name = "Kul Tiran Lumberer's Hatchet", quests = {86764}, coords = "40284188"},
+    {name = "Rune-Seared Spear", quests = {85716}, coords = "57937837"},
     {name = "Stone Carver's Scramseax", quests = {85859}, coords = "26502340"},
     {name = "Unsolved Amethyst Runelock", quests = {86171}, coords = "67557351"},
 }
 
 aura_env.siren_isle_forgotten_vault = {
-    {name = "Glittering Vault Shard", quests = {}, coords = "32137944"},
     {name = "Gunnlod the Sea-Drinker", quests = {84797}, coords = "66465635"},
     {name = "Ksvir the Forgotten", quests = {85406}, coords = "37967648"},
     {name = "Shardsong", quests = {86779}, coords = "28072475"},
